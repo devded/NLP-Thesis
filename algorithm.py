19d8ba41ecbd33116f0d9c2ab785727f2e41fe95
smdedar <thededar@gmail.com>
2020-08-06 02:46:57 +0600

    initial done

--- algorithm.py
+++ algorithm.py
@@ -2,17 +2,18 @@ LogisticRegression(class_weight='balanced')
 DecisionTreeClassifier(class_weight='balanced')
 KNeighborsClassifier()
 SVC(class_weight='balanced')
-
 GradientBoostingClassifier()
 RandomForestClassifier()
-
-
 RidgeClassifier(class_weight='balanced')
 AdaBoostClassifier(n_estimators=100)
 MultinomialNB()
 Perceptron(class_weight='balanced')
 XGBClassifier(n_estimators=300)
 CatBoostClassifier(verbose=0)
+KMeans()
+GaussianNB()
+
+
 
 LogisticRegression(C=1.0)
 SVC()
@@ -20,3 +21,6 @@ MultinomialNB()
 DecisionTreeClassifier()
 KNeighborsClassifier(n_neighbors=5)
 RandomForestClassifier()
+
+
+LogisticRegression(class_weight='balanced'),DecisionTreeClassifier(class_weight='balanced'),KNeighborsClassifier(),SVC(class_weight='balanced'), GradientBoostingClassifier(),RandomForestClassifier(),RidgeClassifier(class_weight='balanced'),AdaBoostClassifier(n_estimators=100),MultinomialNB(),Perceptron(class_weight='balanced'),XGBClassifier(n_estimators=300),CatBoostClassifier(verbose=0),KMeans(),GaussianNB()
